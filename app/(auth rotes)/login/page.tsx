0b0af288a8bc6b1aa363580197eb40409f3842ed
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-08-10 03:20:39 +0300

    Added LoginForm

---
+++ app/(auth rotes)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/LoginForm/LoginForm';
+import Container from '@/components/Container/Container';
+
+const RegisterPage = () => {
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  );
+};
+
+export default RegisterPage;
