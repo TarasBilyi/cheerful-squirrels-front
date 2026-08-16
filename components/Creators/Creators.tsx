132b08d7322b2646a5c96c724c963067a924f23a
Musiienko <mysiienko.vlad14062007@gmail.com>
2026-08-16 22:12:39 +0300

    header

--- components/Creators/Creators.tsx
+++ components/Creators/Creators.tsx
@@ -6,16 +6,23 @@ const Creators = () => {
 return (
     <section className={styles.creators}>
         <Container>
-            <div className={styles.head}>
-            <h2 className={styles.title}>Top Creators</h2>
-                <Link href="/authors" className={styles.linkToCreators}>
-                    <span className={styles.textLink}>Go to all Creators</span>
-                    <svg width="15" height="15" aria-hidden>
-                        <use href="/icons/sprite.svg#top-right" />
-                    </svg>
-                </Link>
-            </div>
-            <ul>
+            <div className={styles.header}>
+          <h2 className={styles.title}>Top Creators</h2>
+
+            <Link href="/authors" className={styles.link}>
+            Go to all Creators
+
+            <svg
+                className={styles.icon}
+                width="15"
+                height="15"
+                aria-hidden="true"
+            >
+                <use href="/icons/sprite.svg#top-right" />
+            </svg>
+            </Link>
+        </div>
+            <ul className={styles.list}>
                 
             </ul>
         </Container>
