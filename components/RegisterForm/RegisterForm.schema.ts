de2c7cd342a22ecc68782177dfa8f0ea237dac0d
TarasBilyi <taras.bilyi@gmail.com>
2026-08-18 02:49:22 +0300

    fix

--- components/RegisterForm/RegisterForm.schema.ts
+++ components/RegisterForm/RegisterForm.schema.ts
@@ -1,13 +1,14 @@
 import * as Yup from 'yup';
-
+import { EMAIL_REGEX } from '@/lib/utils/validation';
 
 export const RegisterSchema = Yup.object({
   name: Yup.string()
+    .transform(value => value?.trim())
     .min(2, 'Name must be at least 2 characters')
     .max(32, 'Name must be at most 32 characters')
     .required('Name is required'),
   email: Yup.string()
-    .email('Enter a valid email')
+    .matches(EMAIL_REGEX, 'Enter a valid email')
     .max(64, 'Email must be at most 64 characters')
     .required('Email is required'),
   password: Yup.string()
